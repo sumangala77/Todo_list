3ba192fc68ac7fa5395d2ee70f7214cb5a03844c
sumangala77 <sumangala2004@gmail.com>
2026-04-26 22:13:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = taskInput.value;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.onclick = function () {
+        taskList.removeChild(li);
+    };
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    taskInput.value = "";
+}
+
